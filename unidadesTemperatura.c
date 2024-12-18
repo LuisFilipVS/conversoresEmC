5dd58a5e5004001cfbad69c52599129bf45230f6
Marco Túlio Macêdo <marcomacedo018@gmail.com>
2024-12-18 16:33:46 -0300

    Estilizando menu temperatura e linkando com o main

--- unidadesTemperatura.c
+++ unidadesTemperatura.c
@@ -1,73 +1,84 @@
 #include <stdio.h>
-
-void escolherConversaoTemperatura();
-
-float celsiusParaFahrenheit(float temperaturaCelsius);
-float celsiusParaKelvin(float temperaturaCelsius);
-
-float fahrenheitParaCelsius(float temperaturaFahrenheit);
-float fahrenheitParaKelvin(float temperaturaFahrenheit);
-
-float kelvinParaCelsius(float temperaturaKelvin);
-float kelvinParaFahrenheit(float temperaturaKelvin);
+#include "conversoes.h"
 
 void escolherConversaoTemperatura() {
     int opcao, validacao = 1;
     float temperatura;
 
     do {
-        printf("\nInsira 1: Celsius -> Fahrenheit\n"
-               "Insira 2: Celsius -> Kelvin\n"
-               "Insira 3: Fahrenheit -> Celsius\n"
-               "Insira 4: Fahrenheit -> Kelvin\n"
-               "Insira 5: Kelvin -> Celsius\n"
-               "Insira 6: Kelvin -> Fahrenheit\n"
-               "Insira 7: Sair\n");
+        printf("\n|_____________Conversor de Temperatura_____________|\n"
+            "|Insira 1: Celsius -> Fahrenheit                   |\n"
+            "|Insira 2: Celsius -> Kelvin                       |\n"
+            "|Insira 3: Fahrenheit -> Celsius                   |\n"
+            "|Insira 4: Fahrenheit -> Kelvin                    |\n"
+            "|Insira 5: Kelvin -> Celsius                       |\n"
+            "|Insira 6: Kelvin -> Fahrenheit                    |\n"
+            "|Insira 7: Voltar                                  |\n"
+            "|__________________________________________________|\n"
+            ">>> ");
         scanf("%d", &opcao);
 
         switch (opcao) {
         case 1:
-            printf("Digite a temperatura em Celsius: ");
+            printf("\nDigite a temperatura em Celsius: ");
             scanf("%f", &temperatura);
-            printf("%.2f ºC em Fahrenheit: %.2f ºF\n", temperatura,
-                   celsiusParaFahrenheit(temperatura));
+            printf(
+                "\n===============================================\n"
+                "%.2f ºC em Fahrenheit: %.2f ºF"
+                "\n===============================================\n",
+                temperatura, celsiusParaFahrenheit(temperatura));
             break;
         case 2:
-            printf("Digite a temperatura em Celsius: ");
+            printf("\nDigite a temperatura em Celsius: ");
             scanf("%f", &temperatura);
-            printf("%.2f ºC em Kelvin: %.2f K\n", temperatura,
-                   celsiusParaKelvin(temperatura));
+            printf(
+                "\n===============================================\n"
+                "%.2f ºC em Kelvin: %.2f K"
+                "\n===============================================\n",
+                temperatura, celsiusParaKelvin(temperatura));
             break;
         case 3:
-            printf("Digite a temperatura em Fahrenheit: ");
+            printf("\nDigite a temperatura em Fahrenheit: ");
             scanf("%f", &temperatura);
-            printf("%.2f ºF em Celsius: %.2f ºC\n", temperatura,
-                   fahrenheitParaCelsius(temperatura));
+            printf(
+                "\n===============================================\n"
+                "%.2f ºF em Celsius: %.2f ºC" 
+                "\n===============================================\n",
+                temperatura, fahrenheitParaCelsius(temperatura));
             break;
         case 4:
-            printf("Digite a temperatura em Fahrenheit: ");
+            printf("\nDigite a temperatura em Fahrenheit: ");
             scanf("%f", &temperatura);
-            printf("%.2f ºF em Kelvin: %.2f K\n", temperatura,
-                   fahrenheitParaKelvin(temperatura));
+            printf(
+                "\n===============================================\n"
+                "%.2f ºF em Kelvin: %.2f K" 
+                "\n===============================================\n",
+                temperatura, fahrenheitParaKelvin(temperatura));
             break;
         case 5:
-            printf("Digite a temperatura em Kelvin: ");
+            printf("\nDigite a temperatura em Kelvin: ");
             scanf("%f", &temperatura);
-            printf("%.2f K em Celcius: %.2f ºC\n", temperatura,
-                   kelvinParaCelsius(temperatura));
+            printf(
+                "\n===============================================\n"
+                "%.2f K em Celcius: %.2f ºC"
+                "\n===============================================\n",
+                temperatura, kelvinParaCelsius(temperatura));
             break;
         case 6:
-            printf("Digite a temperatura em Kelvin: ");
+            printf("\nDigite a temperatura em Kelvin: ");
             scanf("%f", &temperatura);
-            printf("%.2f K em Fahrenheit: %.2f ºF\n", temperatura,
-                   kelvinParaFahrenheit(temperatura));
+            printf(
+                "\n===============================================\n"
+                "%.2f K em Fahrenheit: %.2f ºF" 
+                "\n===============================================\n",
+                temperatura, kelvinParaFahrenheit(temperatura));
             break;
         case 7:
-            printf("Programa executado com sucesso!\n");
             validacao = 0;
             break;
         default:
-            printf("Opção inidisponível :( Tente novamente\n");
+            printf("\nOpção inidisponível :( Tente novamente\n");
+            validacao = 1;
             break;
         }
     } while (validacao);
