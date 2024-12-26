e0c7f3b41950b5d5d066bfe1edff5f568ed4ad27
joabe-nascimento <joabefnascimento1@outlook.com>
2024-12-25 23:43:52 -0300

    ajuste no código

--- Unidadeconversor_area.c
+++ Unidadeconversor_area.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
 
 // Funções para conversão
-double metrosQuadradosParaCentimetrosQuadrados(double metros) {
-    return metros * 10000;
-}
+double metrosQuadradosParaCentimetrosQuadrados(double metros);
+double centimetrosQuadradosParaMetrosQuadrados(double centimetros);
 
-double centimetrosQuadradosParaMetrosQuadrados(double centimetros) {
-    return centimetros / 10000;
-}
 
 int main() {
     int opcao;
     double valor, resultado;
 
-    printf("===== Conversor de Unidades de Área =====\n");
-    printf("1. Metros quadrados para centímetros quadrados\n");
-    printf("2. Centímetros quadrados para metros quadrados\n");
-    printf("Escolha uma opção (1 ou 2): ");
+    printf("===== Conversor de Unidades de Area =====\n");
+    printf("1. Metro(s) quadrados para centimetro(s) quadrados\n");
+    printf("2. Centimetro(s) quadrados para metro(s) quadrados\n");
+    printf("Escolha uma opcao (1 ou 2): ");
     scanf("%d", &opcao);
 
     if (opcao == 1) {
-        printf("Digite o valor em metros quadrados: ");
+        printf("Digite o valor em metro(s) quadrados: ");
         scanf("%lf", &valor);
         resultado = metrosQuadradosParaCentimetrosQuadrados(valor);
-        printf("%.2lf metros quadrados equivalem a %.2lf centímetros quadrados.\n", valor, resultado);
+        printf("%.2lf metro(s) quadrados equivalem a %.2lf centimetro(s) quadrados.\n", valor, resultado);
     } else if (opcao == 2) {
-        printf("Digite o valor em centímetros quadrados: ");
+        printf("Digite o valor em centimetro(s) quadrados: ");
         scanf("%lf", &valor);
         resultado = centimetrosQuadradosParaMetrosQuadrados(valor);
-        printf("%.2lf centímetros quadrados equivalem a %.2lf metros quadrados.\n", valor, resultado);
+        printf("%.2lf centimetro(s) quadrados equivalem a %.2lf metro(s) quadrados.\n", valor, resultado);
     } else {
-        printf("Opção inválida. Por favor, reinicie o programa.\n");
+        printf("Opcao invalida. Por favor, reinicie o programa.\n");
     }
-
     return 0;
+}
+
+double metrosQuadradosParaCentimetrosQuadrados(double metros) {
+    return metros * 100;
+}
+
+double centimetrosQuadradosParaMetrosQuadrados(double centimetros) {
+    return centimetros / 100;
 }
